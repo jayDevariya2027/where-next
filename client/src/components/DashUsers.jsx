60a4ac852af9f24ccc7e6d12f4bc483305e3617f
jaydevariya2027 <jaydevariya2027@gmail.com>
2024-03-21 17:39:49 +0530

    add delete user functionality to the admin user

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -1,18 +1,15 @@
-import { Modal, Table, Button } from "flowbite-react";
-import { useEffect, useState } from "react";
-import { HiOutlineExclamationCircle } from "react-icons/hi";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { FaTimes } from "react-icons/fa";
-import { FaCheck } from "react-icons/fa";
+import { Modal, Table, Button } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { FaCheck, FaTimes } from 'react-icons/fa';
 
 export default function DashUsers() {
   const { currentUser } = useSelector((state) => state.user);
   const [users, setUsers] = useState([]);
   const [showMore, setShowMore] = useState(true);
   const [showModal, setShowModal] = useState(false);
-  const [userIdToDelete, setUserIdToDelete] = useState("");
-
+  const [userIdToDelete, setUserIdToDelete] = useState('');
   useEffect(() => {
     const fetchUsers = async () => {
       try {
@@ -45,50 +42,43 @@ export default function DashUsers() {
         }
       }
     } catch (error) {
-      console.log(error);
+      console.log(error.message);
     }
   };
 
-  //   const handleDeleteUser = async () => {
-  //     setShowModal(false);
-  //     try {
-  //       const res = await fetch(
-  //         `/api/user/deleteuser/${userIdToDelete}/${currentUser._id}`,
-  //         {
-  //           method: "DELETE",
-  //         }
-  //       );
-  //       const data = await res.json();
-  //       if (!res.ok) {
-  //         console.log(data.message);
-  //       } else {
-  //         setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
+  const handleDeleteUser = async () => {
+    try {
+        const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
+            method: 'DELETE',
+        });
+        const data = await res.json();
+        if (res.ok) {
+            setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+            setShowModal(false);
+        } else {
+            console.log(data.message);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+  };
 
   return (
-    <div
-      className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500
-    "
-    >
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && users.length > 0 ? (
         <>
-          <Table hoverable className="shadow-md">
+          <Table hoverable className='shadow-md'>
             <Table.Head>
               <Table.HeadCell>Date created</Table.HeadCell>
               <Table.HeadCell>User image</Table.HeadCell>
-              <Table.HeadCell>User name</Table.HeadCell>
-              <Table.HeadCell>User email</Table.HeadCell>
+              <Table.HeadCell>Username</Table.HeadCell>
+              <Table.HeadCell>Email</Table.HeadCell>
               <Table.HeadCell>Admin</Table.HeadCell>
               <Table.HeadCell>Delete</Table.HeadCell>
             </Table.Head>
-
             {users.map((user) => (
-              <Table.Body className="divide-y" key={user._id}>
-                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <Table.Body className='divide-y' key={user._id}>
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
                   <Table.Cell>
                     {new Date(user.createdAt).toLocaleDateString()}
                   </Table.Cell>
@@ -96,25 +86,25 @@ export default function DashUsers() {
                     <img
                       src={user.profilePicture}
                       alt={user.username}
-                      className="w-10 h-10 object-cover bg-gray-500 rounded-full "
+                      className='w-10 h-10 object-cover bg-gray-500 rounded-full'
                     />
                   </Table.Cell>
                   <Table.Cell>{user.username}</Table.Cell>
                   <Table.Cell>{user.email}</Table.Cell>
                   <Table.Cell>
                     {user.isAdmin ? (
-                      <FaCheck className="text-green-500" />
+                      <FaCheck className='text-green-500' />
                     ) : (
-                      <FaTimes className="text-red-500" />
+                      <FaTimes className='text-red-500' />
                     )}
                   </Table.Cell>
                   <Table.Cell>
                     <span
-                      className="text-red-500 font-medium hover:underline cursor-pointer"
                       onClick={() => {
                         setShowModal(true);
                         setUserIdToDelete(user._id);
                       }}
+                      className='font-medium text-red-500 hover:underline cursor-pointer'
                     >
                       Delete
                     </span>
@@ -126,7 +116,7 @@ export default function DashUsers() {
           {showMore && (
             <button
               onClick={handleShowMore}
-              className="w-full text-teal-500 self-center text-sm py-7"
+              className='w-full text-teal-500 self-center text-sm py-7'
             >
               Show more
             </button>
@@ -135,25 +125,24 @@ export default function DashUsers() {
       ) : (
         <p>You have no users yet!</p>
       )}
-
       <Modal
         show={showModal}
-        popup
         onClose={() => setShowModal(false)}
-        size="md"
+        popup
+        size='md'
       >
         <Modal.Header />
         <Modal.Body>
-          <div className="text-center">
-            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
-            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+          <div className='text-center'>
+            <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+            <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
               Are you sure you want to delete this user?
             </h3>
-            <div className="flex justify-center gap-4">
-              <Button color="failure" onClick={{}}>
+            <div className='flex justify-center gap-4'>
+              <Button color='failure' onClick={handleDeleteUser}>
                 Yes, I'm sure
               </Button>
-              <Button color="gray" onClick={() => setShowModal(false)}>
+              <Button color='gray' onClick={() => setShowModal(false)}>
                 No, cancel
               </Button>
             </div>
