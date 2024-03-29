abca74886109cba8b50cd20503288640337b7349
jaydevariya2027 <jaydevariya2027@gmail.com>
2024-03-29 16:29:46 +0530

    updated search functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,5 +1,5 @@
-import Post from '../models/post.model.js';
-import { errorHandler } from '../utils/error.js';
+import Post from "../models/post.model.js";
+import { errorHandler } from "../utils/error.js"
 
 export const create = async (req, res, next) => {
     if (!req.user.isAdmin) {
@@ -18,6 +18,7 @@ export const create = async (req, res, next) => {
         slug,
         userId: req.user.id,
     });
+
     try {
         const savedPost = await newPost.save();
         res.status(201).json(savedPost);
@@ -26,11 +27,11 @@ export const create = async (req, res, next) => {
     }
 };
 
-export const getposts = async (req, res, next) => {
+export const getPosts = async (req, res, next) => {
     try {
         const startIndex = parseInt(req.query.startIndex) || 0;
         const limit = parseInt(req.query.limit) || 9;
-        const sortDirection = req.query.order === 'asc' ? 1 : -1;
+        const sortDirection = req.query.sort === 'asc' ? 1 : -1; // Updated this line
         const posts = await Post.find({
             ...(req.query.userId && { userId: req.query.userId }),
             ...(req.query.category && { category: req.query.category }),
@@ -39,37 +40,28 @@ export const getposts = async (req, res, next) => {
             ...(req.query.searchTerm && {
                 $or: [
                     { title: { $regex: req.query.searchTerm, $options: 'i' } },
-                    { content: { $regex: req.query.searchTerm, $options: 'i' } },
-                ],
+                    { content: { $regex: req.query.searchTerm, $options: 'i' } }
+                ]
             }),
-        })
-            .sort({ updatedAt: sortDirection })
-            .skip(startIndex)
-            .limit(limit);
+        }).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit);
 
         const totalPosts = await Post.countDocuments();
-
         const now = new Date();
-
         const oneMonthAgo = new Date(
             now.getFullYear(),
             now.getMonth() - 1,
             now.getDate()
         );
-
         const lastMonthPosts = await Post.countDocuments({
-            createdAt: { $gte: oneMonthAgo },
-        });
+            createdAt: { $gte: oneMonthAgo }
+        })
 
-        res.status(200).json({
-            posts,
-            totalPosts,
-            lastMonthPosts,
-        });
+        res.status(200).json({ posts, totalPosts, lastMonthPosts });
     } catch (error) {
         next(error);
     }
 };
+
 
 export const deletepost = async (req, res, next) => {
     if (!req.user.isAdmin || req.user.id !== req.params.userId) {
@@ -77,31 +69,27 @@ export const deletepost = async (req, res, next) => {
     }
     try {
         await Post.findByIdAndDelete(req.params.postId);
-        res.status(200).json('The post has been deleted');
+        res.status(200).json("The post has been deleted");
     } catch (error) {
         next(error);
     }
-};
+}
 
 export const updatepost = async (req, res, next) => {
     if (!req.user.isAdmin || req.user.id !== req.params.userId) {
         return next(errorHandler(403, 'You are not allowed to update this post'));
     }
     try {
-        const updatedPost = await Post.findByIdAndUpdate(
-            req.params.postId,
-            {
-                $set: {
-                    title: req.body.title,
-                    content: req.body.content,
-                    category: req.body.category,
-                    image: req.body.image,
-                },
-            },
-            { new: true }
-        );
+        const updatedPost = await Post.findByIdAndUpdate(req.params.postId, {
+            $set: {
+                title: req.body.title,
+                content: req.body.content,
+                image: req.body.image,
+                category: req.body.category
+            }
+        }, { new: true });
         res.status(200).json(updatedPost);
     } catch (error) {
         next(error);
     }
-};
+}
--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,13 +1,12 @@
-import { Button, Select, TextInput } from 'flowbite-react';
-import { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import PostCard from '../components/PostCard';
+import { Button, Select, TextInput } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import PostCard from "../components/PostCard";
 
 export default function Search() {
   const [sidebarData, setSidebarData] = useState({
-    searchTerm: '',
-    sort: 'desc',
-    category: 'uncategorized',
+    searchTerm: "",
+    category: "uncategorized",
   });
 
   console.log(sidebarData);
@@ -21,14 +20,12 @@ export default function Search() {
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const searchTermFromUrl = urlParams.get('searchTerm');
-    const sortFromUrl = urlParams.get('sort');
-    const categoryFromUrl = urlParams.get('category');
-    if (searchTermFromUrl || sortFromUrl || categoryFromUrl) {
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    const categoryFromUrl = urlParams.get("category");
+    if (searchTermFromUrl || categoryFromUrl) {
       setSidebarData({
         ...sidebarData,
         searchTerm: searchTermFromUrl,
-        sort: sortFromUrl,
         category: categoryFromUrl,
       });
     }
@@ -56,15 +53,11 @@ export default function Search() {
   }, [location.search]);
 
   const handleChange = (e) => {
-    if (e.target.id === 'searchTerm') {
+    if (e.target.id === "searchTerm") {
       setSidebarData({ ...sidebarData, searchTerm: e.target.value });
     }
-    if (e.target.id === 'sort') {
-      const order = e.target.value || 'desc';
-      setSidebarData({ ...sidebarData, sort: order });
-    }
-    if (e.target.id === 'category') {
-      const category = e.target.value || 'uncategorized';
+    if (e.target.id === "category") {
+      const category = e.target.value || "uncategorized";
       setSidebarData({ ...sidebarData, category });
     }
   };
@@ -72,18 +65,22 @@ export default function Search() {
   const handleSubmit = (e) => {
     e.preventDefault();
     const urlParams = new URLSearchParams(location.search);
-    urlParams.set('searchTerm', sidebarData.searchTerm);
-    urlParams.set('sort', sidebarData.sort);
-    urlParams.set('category', sidebarData.category);
+    urlParams.set("searchTerm", sidebarData.searchTerm);
+    urlParams.set("category", sidebarData.category);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
+    setSidebarData({
+      ...sidebarData,
+      searchTerm: "",
+      category: "uncategorized",
+    });
   };
 
   const handleShowMore = async () => {
     const numberOfPosts = posts.length;
     const startIndex = numberOfPosts;
     const urlParams = new URLSearchParams(location.search);
-    urlParams.set('startIndex', startIndex);
+    urlParams.set("startIndex", startIndex);
     const searchQuery = urlParams.toString();
     const res = await fetch(`/api/post/getposts?${searchQuery}`);
     if (!res.ok) {
@@ -101,62 +98,56 @@ export default function Search() {
   };
 
   return (
-    <div className='flex flex-col md:flex-row'>
-      <div className='p-7 border-b md:border-r md:min-h-screen border-gray-500'>
-        <form className='flex flex-col gap-8' onSubmit={handleSubmit}>
-          <div className='flex   items-center gap-2'>
-            <label className='whitespace-nowrap font-semibold'>
+    <div className="flex flex-col md:flex-row">
+      <div className="p-7 border-b md:border-r md:min-h-screen border-gray-500">
+        <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
+          <div className="flex   items-center gap-2">
+            <label className="whitespace-nowrap font-semibold">
               Search Term:
             </label>
             <TextInput
-              placeholder='Search...'
-              id='searchTerm'
-              type='text'
+              placeholder="Search..."
+              id="searchTerm"
+              type="text"
               value={sidebarData.searchTerm}
               onChange={handleChange}
             />
           </div>
-          <div className='flex items-center gap-2'>
-            <label className='font-semibold'>Sort:</label>
-            <Select onChange={handleChange} value={sidebarData.sort} id='sort'>
-              <option value='desc'>Latest</option>
-              <option value='asc'>Oldest</option>
-            </Select>
-          </div>
-          <div className='flex items-center gap-2'>
-            <label className='font-semibold'>Category:</label>
+
+          <div className="flex items-center gap-2">
+            <label className="font-semibold">Category:</label>
             <Select
               onChange={handleChange}
               value={sidebarData.category}
-              id='category'
+              id="category"
             >
-              <option value='uncategorized'>Uncategorized</option>
-              <option value='reactjs'>React.js</option>
-              <option value='nextjs'>Next.js</option>
-              <option value='javascript'>JavaScript</option>
+              <option value="uncategorized">Uncategorized</option>
+              <option value="reactjs">React.js</option>
+              <option value="nextjs">Next.js</option>
+              <option value="javascript">JavaScript</option>
             </Select>
           </div>
-          <Button type='submit' outline gradientDuoTone='purpleToPink'>
+          <Button type="submit" outline gradientDuoTone="purpleToPink">
             Apply Filters
           </Button>
         </form>
       </div>
-      <div className='w-full'>
-        <h1 className='text-3xl font-semibold sm:border-b border-gray-500 p-3 mt-5 '>
+      <div className="w-full">
+        <h1 className="text-3xl font-semibold sm:border-b border-gray-500 p-3 mt-5 ">
           Posts results:
         </h1>
-        <div className='p-7 flex flex-wrap gap-4'>
+        <div className="p-7 flex flex-wrap gap-4">
           {!loading && posts.length === 0 && (
-            <p className='text-xl text-gray-500'>No posts found.</p>
+            <p className="text-xl text-gray-500">No posts found.</p>
           )}
-          {loading && <p className='text-xl text-gray-500'>Loading...</p>}
+          {loading && <p className="text-xl text-gray-500">Loading...</p>}
           {!loading &&
             posts &&
             posts.map((post) => <PostCard key={post._id} post={post} />)}
           {showMore && (
             <button
               onClick={handleShowMore}
-              className='text-teal-500 text-lg hover:underline p-7 w-full'
+              className="text-teal-500 text-lg hover:underline p-7 w-full"
             >
               Show More
             </button>
